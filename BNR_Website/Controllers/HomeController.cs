abeed3e89498099dd2db90ed9a770cd3725282da
Travis Higgs <Travis Higgs@TRAVISHIGGS-PC>
2017-02-21 22:02:08 -0500

    Readded aurelia and sass, and foundation

--- BNR_Website/Controllers/HomeController.cs
+++ BNR_Website/Controllers/HomeController.cs
@@ -10,21 +10,7 @@ namespace BNR_Website.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return new FilePathResult("~/index.html", "text/html");
         }
     }
 }
