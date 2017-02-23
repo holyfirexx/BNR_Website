11d559cd365ebcb5dd6ed66a0ae811814971f2e1
MinchMiester <minch.bradley@gmail.com>
2017-02-22 21:20:14 -0500

    log4net implemented. Global.asax file structured.

--- BNR_Website/Global.asax.cs
+++ BNR_Website/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,60 @@ using System.Web.Routing;
 
 namespace BNR_Website
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(MvcApplication));
+
+
         protected void Application_Start()
+        {
+            try
+            {
+                XmlConfigurator.Configure();
+                ConfigureRegistrations();
+            }
+            catch(Exception ex)
+            {
+                log.Fatal("Application failed to start.", ex.InnerException);
+                throw;
+            }
+
+        }
+
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+            Session["DasDevs"] = true;
+            log.Error("hi");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                //if stuff is localhost only on session start do it here
+            }
+        }
+
+        protected void Session_End(Object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+        }
+
+        protected void Application_Error()
+        {
+            HttpException lastErrorWrapper = Server.GetLastError() as HttpException;
+
+            Exception lastError = lastErrorWrapper;
+            if (lastErrorWrapper.InnerException != null) lastError = lastErrorWrapper.InnerException;
+
+            string lastErrorTypeName = lastError.GetType().ToString();
+            string lastErrorMessage = lastError.Message;
+            string lastErrorStackTrace = lastError.StackTrace;
+
+            log.Error("Unhandled application exception. Error Type Name: " + lastErrorTypeName + Environment.NewLine +
+                "Error Message: " + lastErrorMessage + Environment.NewLine +
+                "Error Stack Trace: " + lastErrorStackTrace);
+        }
+
+        private static void ConfigureRegistrations()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
